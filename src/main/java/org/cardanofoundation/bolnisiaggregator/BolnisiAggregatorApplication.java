3ab638073470356eaae996cebdf6b67a9e2249e9
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2024-09-26 13:39:26 +0200

    first commit

---
+++ src/main/java/org/cardanofoundation/bolnisiaggregator/BolnisiAggregatorApplication.java
@@ -0,0 +1,13 @@
+package org.cardanofoundation.bolnisiaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolnisiAggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BolnisiAggregatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cardanofoundation/bolnisiaggregator/BolnisiAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cardanofoundation.bolnisiaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolnisiAggregatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
